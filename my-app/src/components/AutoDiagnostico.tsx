0259f05722e27c0a4c489e77c048e8f188dcbb5e
lukashibuya <uraluka@uol.com.br>
2024-09-16 19:40:40 -0300

    Conteudo da pagina de auto diagnostico

---
+++ my-app/src/components/AutoDiagnostico.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { BackgroundDiv } from '../styles/MainContentStyle';
+import { Formulario } from '../styles/AutoDIagnosticoStyle';
+
+const AutoDiagnostico: React.FC = () => {
+  const navigate = useNavigate();
+
+
+  useEffect(() => {
+    const email = window.localStorage.getItem('email');
+
+    if (!email) {
+
+      navigate('/perfil');
+    }
+  }, [navigate]);
+
+  return (
+    <BackgroundDiv>
+      <Formulario>
+        <div className="conteudo">
+          <h1>Auto Diagnóstico</h1>
+          <form>
+            <label htmlFor="marca">Marca:</label>
+            <input type="text" id="marca" name="marca" />
+
+            <label htmlFor="modelo">Modelo:</label>
+            <input type="text" id="modelo" name="modelo" />
+
+            <label htmlFor="problema">Identifique seu problema:</label>
+            <input type="text" id="problema" name="problema" />
+
+            <button type="submit">ENVIAR</button>
+          </form>
+        </div>
+      </Formulario>
+    </BackgroundDiv>
+  );
+};
+
+export default AutoDiagnostico;
---
+++ my-app/src/styles/AutoDiagnosticoStyle.ts
@@ -0,0 +1,63 @@
+import styled from 'styled-components';
+import backgroundImage from '../assets/img/background.jpg';
+
+export const BackgroundDiv = styled.div
+    background-image: url(${backgroundImage});
+    font-family: "Aleo", sans-serif;
+    margin: 0;
+    padding: 0;
+    background-size: cover; 
+    background-repeat: no-repeat;
+    background-position: center; 
+;
+export const Formulario = styled.form
+display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    padding: 2rem;
+    background-color: #fff;
+    border-radius: 4px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    max-width: 600px;
+    margin: 5rem auto;
+    border: 2px solid black;
+    height: 500px;
+
+h1 {
+    text-align: center;
+    margin-bottom: 2rem;
+    color: #0088CC;
+}
+
+label {
+    display: flex;
+    margin-bottom: 0.5rem;
+    font-weight: bold;
+    color: #0088CC;
+    font-family: "Aleo";
+}
+
+input {
+    width: 100%;
+    padding: 0.5rem;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    margin-bottom: 1rem;
+}
+
+button[type="submit"] {
+    padding: 0.5rem 2rem;
+    background-color: #00A1FC;
+    color: #fff;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    margin-left: 200px;
+}
+
+button[type="submit"]:hover {
+    background-color: #0088CC;
+}
+
+;
